ccdef584317b19f147280590ec1fc51dcc9565cc
dhruvmoradiya03 <dhruv.m@squareinfosoft.com>
2025-12-01 16:13:09 +0530

    bug fix

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import Sidebar from "@/components/sidebar";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("idToken") : null;
+
+    if (!token) {
+      router.replace("/login");
+    }
+  }, [router]);
+
   return (
     <div className="flex">
       <Sidebar />
